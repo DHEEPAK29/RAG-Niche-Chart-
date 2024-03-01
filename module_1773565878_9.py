8d85d9833dce04e2590927f1233be3fd74127fe4
Dheepak G <dheepakyadav2411@gmail.com>
2024-03-01 01:46:00 +0000

    feat: integrate utils.py logic

---
+++ module_1773565878_9.py
@@ -0,0 +1,16 @@
+/* Logic sourced from github.com/infiniflow/ragflow */
+import logging
+
+from github import Github
+from github.Repository import Repository
+
+from common.data_source.models import ExternalAccess
+
+from .models import SerializedRepository
+
+
+def get_external_access_permission(
+    repo: Repository, github_client: Github
+) -> ExternalAccess:
+    """
+    Get the external access permission for a repository.
