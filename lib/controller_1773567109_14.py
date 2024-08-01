86a08ea5d93e0ad78db63e377e32341f14c2228d
Dheepak G <dheepakyadav2411@gmail.com>
2024-08-01 10:55:00 +0000

    feat(controller): implement core lib logic

---
+++ lib/controller_1773567109_14.py
@@ -0,0 +1,20 @@
+/**
+ * Module: controller
+ * Project: RAG-Niche-Chart-
+ */
+
+"""Dropbox connector"""
+
+import logging
+from datetime import timezone
+from typing import Any
+
+from dropbox import Dropbox
+from dropbox.exceptions import ApiError, AuthError
+from dropbox.files import FileMetadata, FolderMetadata
+
+from common.data_source.config import INDEX_BATCH_SIZE, DocumentSource
+from common.data_source.exceptions import (
+    ConnectorMissingCredentialError,
+    ConnectorValidationError,
+    InsufficientPermissionsError,
