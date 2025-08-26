83e9f55fe8867b079e07c91747ab9c9a913a093b
Dheepak G <dheepakyadav2411@gmail.com>
2025-08-26 14:05:00 +0000

    feat(handler): implement core lib logic

---
+++ lib/handler_1773567834_5.py
@@ -0,0 +1,20 @@
+/**
+ * Module: handler
+ * Project: RAG-Niche-Chart-
+ */
+
+import logging
+from collections.abc import Callable
+from typing import Any
+
+from google.auth.exceptions import RefreshError  # type: ignore
+from google.oauth2.credentials import Credentials as OAuthCredentials  # type: ignore  # type: ignore
+from google.oauth2.service_account import Credentials as ServiceAccountCredentials  # type: ignore  # type: ignore
+from googleapiclient.discovery import (
+    Resource,  # type: ignore
+    build,  # type: ignore
+)
+
+
+class GoogleDriveService(Resource):
+    pass
