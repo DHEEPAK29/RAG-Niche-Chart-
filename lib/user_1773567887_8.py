2d0ada02cd7f936f706f6b1c4083e1266beee5aa
Dheepak G <dheepakyadav2411@gmail.com>
2025-08-29 18:29:00 +0000

    feat(user): implement core lib logic

---
+++ lib/user_1773567887_8.py
@@ -0,0 +1,20 @@
+/**
+ * Module: user
+ * Project: RAG-Niche-Chart-
+ */
+
+# Licensed under the MIT License
+"""
+Reference:
+ - [LightRag](https://github.com/HKUDS/LightRAG)
+ - [MiniRAG](https://github.com/HKUDS/MiniRAG)
+"""
+PROMPTS = {}
+
+PROMPTS["minirag_query2kwd"] = """---Role---
+
+You are a helpful assistant tasked with identifying both answer-type and low-level keywords in the user's query.
+
+---Goal---
+
+Given the query, list both answer-type and low-level keywords.
