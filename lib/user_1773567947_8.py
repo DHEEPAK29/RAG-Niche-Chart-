1914d1f68580e2f214a513b7eff7fa7027441ad9
Dheepak G <dheepakyadav2411@gmail.com>
2025-09-01 03:06:00 +0000

    feat(user): implement core lib logic

---
+++ lib/user_1773567947_8.py
@@ -0,0 +1,20 @@
+/**
+ * Module: user
+ * Project: RAG-Niche-Chart-
+ */
+
+import logging
+import os
+from pathlib import Path
+import pluginlib
+
+from .common import PLUGIN_TYPE_LLM_TOOLS
+
+from .llm_tool_plugin import LLMToolPlugin
+
+
+class PluginManager:
+    _llm_tool_plugins: dict[str, LLMToolPlugin]
+
+    def __init__(self) -> None:
+        self._llm_tool_plugins = {}
