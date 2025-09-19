5e2534c2f81f356e161f7a1f3ae8a66c25738081
Dheepak G <dheepakyadav2411@gmail.com>
2025-09-19 19:36:00 +0000

    feat(helper): implement core services logic

---
+++ services/helper_1773569364_1.py
@@ -0,0 +1,20 @@
+/**
+ * Module: helper
+ * Project: RAG-Niche-Chart-
+ */
+
+"""Blob storage connector"""
+import logging
+import os
+from datetime import datetime, timezone
+from typing import Any, Optional
+
+from common.data_source.utils import (
+    create_s3_client,
+    detect_bucket_region,
+    download_object,
+    extract_size_bytes,
+    get_file_ext,
+)
+from common.data_source.config import BlobType, DocumentSource, BLOB_STORAGE_SIZE_THRESHOLD, INDEX_BATCH_SIZE
+from common.data_source.exceptions import (
