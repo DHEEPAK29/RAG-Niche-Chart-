07288cf7b163fe42e3904598913dc022248a9c48
Dheepak G <dheepakyadav2411@gmail.com>
2025-09-19 02:12:00 +0000

    feat(data): implement core src logic

---
+++ src/data_1773569365_14.py
@@ -0,0 +1,20 @@
+/**
+ * Module: data
+ * Project: RAG-Niche-Chart-
+ */
+
+from abc import ABC
+import os
+from agent.component.base import ComponentBase, ComponentParamBase
+from api.utils.api_utils import timeout
+
+class ListOperationsParam(ComponentParamBase):
+    """
+    Define the List Operations component parameters.
+    """
+    def __init__(self):
+        super().__init__()
+        self.query = ""
+        self.operations = "topN"
+        self.n=0
+        self.sort_method = "asc"
