bdc2c9f3d59a0cc6302ab8ca11db4e1c1e1c9d45
Dheepak G <dheepakyadav2411@gmail.com>
2025-10-13 11:24:00 +0000

    feat(user): implement core core logic

---
+++ core/user_1773569868_20.py
@@ -0,0 +1,19 @@
+/**
+ * Module: user
+ * Project: RAG-Niche-Chart-
+ */
+
+import logging
+import re
+from copy import copy
+from dataclasses import dataclass
+from io import BytesIO
+from typing import IO
+
+import bs4
+
+from common.data_source.config import HTML_BASED_CONNECTOR_TRANSFORM_LINKS_STRATEGY, \
+    HtmlBasedConnectorTransformLinksStrategy, WEB_CONNECTOR_IGNORED_CLASSES, WEB_CONNECTOR_IGNORED_ELEMENTS, \
+    PARSE_WITH_TRAFILATURA
+
+MINTLIFY_UNWANTED = ["sticky", "hidden"]
