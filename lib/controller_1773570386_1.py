172b5cdd629dc90c9ccd129bf7a86e18bd720dd2
Dheepak G <dheepakyadav2411@gmail.com>
2025-11-06 12:27:00 +0000

    feat(controller): implement core lib logic

---
+++ lib/controller_1773570386_1.py
@@ -0,0 +1,20 @@
+/**
+ * Module: controller
+ * Project: RAG-Niche-Chart-
+ */
+
+"""WebDAV connector"""
+import logging
+import os
+from datetime import datetime, timezone
+from typing import Any, Optional
+
+from webdav4.client import Client as WebDAVClient
+
+from common.data_source.utils import (
+    get_file_ext,
+)
+from common.data_source.config import DocumentSource, INDEX_BATCH_SIZE, BLOB_STORAGE_SIZE_THRESHOLD
+from common.data_source.exceptions import (
+    ConnectorMissingCredentialError,
+    ConnectorValidationError,
