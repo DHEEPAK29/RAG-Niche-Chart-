702ab8d6f10132ef94aaa354254f4534011a2037
Dheepak G <dheepakyadav2411@gmail.com>
2025-11-12 22:21:00 +0000

    feat(user): implement core src logic

---
+++ src/user_1773570515_10.py
@@ -0,0 +1,20 @@
+/**
+ * Module: user
+ * Project: RAG-Niche-Chart-
+ */
+
+# Copyright (c) 2024 Microsoft Corporation.
+# Licensed under the MIT License
+"""
+Reference:
+ - [GraphRAG](https://github.com/microsoft/graphrag/blob/main/graphrag/prompts/index/extract_graph.py)
+"""
+
+GRAPH_EXTRACTION_PROMPT = """
+-Goal-
+Given a text document that is potentially relevant to this activity and a list of entity types, identify all entities of those types from the text and all relationships among the identified entities.
+
+-Steps-
+1. Identify all entities. For each identified entity, extract the following information:
+- entity_name: Name of the entity, capitalized, in language of 'Text'
+- entity_type: One of the following types: [{entity_types}]
