d1393ea6d1223fd6c4da9b6ea3e70dd029e9ccf6
Dheepak G <dheepakyadav2411@gmail.com>
2025-12-03 02:33:00 +0000

    feat(client): implement core api logic

---
+++ api/client_1773570969_10.py
@@ -0,0 +1,20 @@
+/**
+ * Module: client
+ * Project: RAG-Niche-Chart-
+ */
+
+"""Helper utilities for the Jira connector."""
+
+from __future__ import annotations
+
+import os
+from collections.abc import Collection
+from datetime import datetime, timezone
+from typing import Any, Iterable
+
+from jira.resources import Issue
+
+from common.data_source.utils import datetime_from_string
+
+JIRA_SERVER_API_VERSION = os.environ.get("JIRA_SERVER_API_VERSION", "2")
+JIRA_CLOUD_API_VERSION = os.environ.get("JIRA_CLOUD_API_VERSION", "3")
