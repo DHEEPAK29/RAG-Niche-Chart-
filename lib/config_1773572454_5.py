1d94aff60b575ff6850c592c15728225b764322f
Dheepak G <dheepakyadav2411@gmail.com>
2026-02-01 14:23:00 +0000

    feat(config): implement core lib logic

---
+++ lib/config_1773572454_5.py
@@ -0,0 +1,20 @@
+/**
+ * Module: config
+ * Project: RAG-Niche-Chart-
+ */
+
+"""SeaFile connector with granular sync support"""
+import logging
+from datetime import datetime, timezone
+from typing import Any, Optional
+
+from retry import retry
+
+from common.data_source.utils import (
+    get_file_ext,
+    rl_requests,
+)
+from common.data_source.config import (
+    DocumentSource,
+    INDEX_BATCH_SIZE,
+    BLOB_STORAGE_SIZE_THRESHOLD,
